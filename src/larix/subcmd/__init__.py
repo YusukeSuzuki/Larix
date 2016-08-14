e1a69725ecbf3f248f24263fd0ad2417296f921d
Yusuke Suzuki <yusuke@geekfield.jp>
2016-08-14 20:16:25 +0900

    rm old subcmd

--- src/larix/subcmd/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-def modules():
-    pyfiles = Path(__file__).parent.glob('*.py')
-    return [path.name[:-3] for path in pyfiles if path.is_file() and path != Path(__file__)]
-
-__all__ = modules()
-
